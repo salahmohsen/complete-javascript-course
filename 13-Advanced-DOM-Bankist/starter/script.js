9cd3afe7b0c860a1e3ea6545b792e4bc067c3bd5
salah mohsen <salahmmohsen@gmail.com>
2023-01-29 17:10:03 +0200

    212. Coding Challenge #1 & Adding Challenges PDF file

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -222,15 +222,3 @@ const slider = (function () {
 
   init();
 })();
-
-document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built!', e);
-});
-window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e);
-});
-window.addEventListener('beforeunload', function (e) {
-  e.preventDefault();
-  console.log(e);
-  e.returnValue = '';
-});
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// 212. Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
+};
+
+const bmw = new Car('BMW', 120);
+
+bmw.accelerate();
+bmw.brake();
