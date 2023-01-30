d818bf1bd779a548896ee7430b6e030ee083332e
salah mohsen <salahmmohsen@gmail.com>
2023-01-30 22:07:05 +0200

    217. Coding Challenge #2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -2,22 +2,51 @@
 
 // 212. Coding Challenge #1
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
+// };
+
+// const bmw = new Car('BMW', 120);
+
+// bmw.accelerate();
+// bmw.brake();
+
+///////////////////////////////////////////////////////////
+// 217. Coding Challenge #2
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(Speed) {
+    this.speed = Speed * 1.6;
+  }
+}
 
 const bmw = new Car('BMW', 120);
 
-bmw.accelerate();
-bmw.brake();
+const Ford = new Car('Ford', 120);
+Ford.speedUS = 40;
+console.log(Ford);
