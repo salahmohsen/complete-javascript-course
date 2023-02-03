4a1a67eff082a34bdd1206021b45e8b5c1bc97ae
salah mohsen <salahmmohsen@gmail.com>
2023-02-03 19:13:06 +0200

    220. Inheritance Between "Classes": ES6 Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -84,39 +84,71 @@
 // console.log(salah instanceof Object);
 
 /// Coding Challenge #3
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.make, 'Speed After Acceleration', this.speed, 'Km/h');
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.make, 'Speed After Brake', this.speed, 'Km/h');
+// };
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
-// Link with Car object
-EV.prototype = Object.create(Car.prototype);
-///
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge = (parseFloat(this.charge) / 100 - 0.01) * 100 + '%';
-  console.log(
-    `${this.make} going at ${this.speed}, with a charge of ${this.charge}`
-  );
-};
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+// // Link with Car object
+// EV.prototype = Object.create(Car.prototype);
+// ///
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge = (parseFloat(this.charge) / 100 - 0.01) * 100 + '%';
+//   console.log(
+//     `${this.make} going at ${this.speed}, with a charge of ${this.charge}`
+//   );
+// };
 
-const tesla = new EV('Tesla', 120, '23%');
-tesla.accelerate();
-tesla.brake();
+// const tesla = new EV('Tesla', 120, '23%');
+// tesla.accelerate();
+// tesla.brake();
+
+///////////////////////////////////////////////////////
+// 220. Inheritance Between "Classes": ES6 Classes ////
+///////////////////////////////////////////////////////
+
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    const date = new Date();
+    return date.getFullYear() - this.birthYear;
+  }
+  introduce() {
+    console.log(
+      `My name is ${this.firstName}, i'm ${this.calcAge()} and i'm studying ${
+        this.course
+      }`
+    );
+  }
+}
+
+class Student extends Person {
+  constructor(firstName, birthYear, course) {
+    super(firstName, birthYear);
+    this.course = course;
+  }
+}
+
+const salah = new Student('salah', 1995, 'computer science');
+salah.introduce();
