cf1498185cbcd5721ff6e37c0f094961a781c185
salah mohsen <salahmmohsen@gmail.com>
2023-02-08 14:22:46 +0200

    238. Managing Workout Data: Creating Classes

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,41 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = Date.now().toString().slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain, speed) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence, pace) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this;
+  }
+}
+
+//// App Structure
 class App {
   #map;
   #mapEvent;
