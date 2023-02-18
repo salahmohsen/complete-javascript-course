75e3d53736904e65f39fd9fd868ed25f2273dbdc
salah mohsen <salahmmohsen@gmail.com>
2023-02-18 19:27:01 +0200

    250. Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,28 +5,44 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const renderCountry = function (data, className = '') {
+  //console.log(data);
+  // prettier-ignore
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)}M</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+  </div>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 const getCountry = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
   request.addEventListener('load', function () {
-    const data = JSON.parse(this.responseText);
-    // prettier-ignore
-    const html = `
-    <article class="country">
-    <img class="country__img" src="${data[0].flags.svg}" />
-    <div class="country__data">
-        <h3 class="country__name">${data[0].name.common}</h3>
-        <h4 class="country__region">${data[0].region}</h4>
-        <p class="country__row"><span>👫</span>${(data[0].population / 1000000).toFixed(2)}M</p>
-        <p class="country__row"><span>🗣️</span>${Object.values(data[0].languages)}</p>
-        <p class="country__row"><span>💰</span>${Object.values(data[0].currencies)[0].name}</p>
-    </div>
-  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+    data.borders?.forEach(borderCountry => {
+      const request2 = new XMLHttpRequest();
+      // prettier-ignore
+      request2.open('GET',`https://restcountries.com/v3.1/alpha/${borderCountry}`);
+      request2.send();
+      request2.addEventListener('load', function () {
+        const [data2] = JSON.parse(this.responseText);
+        renderCountry(data2, 'neighbour');
+      });
+    });
   });
 };
 
-getCountry('germany');
-getCountry('Egypt');
+getCountry('usa');
