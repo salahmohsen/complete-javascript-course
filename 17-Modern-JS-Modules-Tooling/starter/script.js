02ad53212f442e8649b031237f8c41a4b353dc4f
salah mohsen <salahmmohsen@gmail.com>
2023-03-06 21:41:54 +0200

    278. Bundling With Parcel and NPM Scripts

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,34 +1,34 @@
-// console.log('second ecerytime after import');
-// import './shoppingCart.js';
+console.log('second ecerytime after import');
+import './shoppingCart.js';
 
-// const shoppingCart = (function () {
-//   const cart = [];
-//   const shippingCost = 10;
-//   const totalPrice = 237;
-//   const totalQuantity = 23;
+const shoppingCart = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
 
-//   const addToCart = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} added to cart`);
-//   };
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
 
-//   const orderStock = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} ordered from supplier`);
-//   };
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
 
-//   return {
-//     addToCart,
-//     cart,
-//     totalPrice,
-//     totalQuantity,
-//   };
-// })();
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
 
-// shoppingCart.addToCart('apple', 4);
-// console.log(shoppingCart);
+shoppingCart.addToCart('apple', 4);
+console.log(shoppingCart);
 
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -45,3 +45,7 @@ const stateDeepClone = cloneDeep(state);
 state.user.loggedIn = false;
 console.log(state);
 console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
