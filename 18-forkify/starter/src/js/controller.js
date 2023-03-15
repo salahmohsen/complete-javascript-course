f85ac4d06be0ba0a3b0c858ec213a17a951934be
salahmohsen <salahmmohsen@gmail.com>
2023-03-15 16:18:32 +0200

    296. Implementing Search Results - Part 1

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -2,6 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './Views/recipeView';
+import searchView from './Views/searchView';
 
 const controlRecipes = async function () {
   try {
@@ -18,15 +19,18 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
-    await model.loadSearchResults('pizza');
+    const query = searchView.getQuery();
+    if (!query) return;
+    await model.loadSearchResults(query);
     console.log(model.state.search.results);
   } catch (error) {
     console.log(error);
   }
 };
-controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
