4995cd225fd03791796fef2cad9924df3316851d
salahmohsen <salahmmohsen@gmail.com>
2023-03-25 14:50:11 +0200

    set page counter to 1 after every new search

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -44,6 +44,7 @@ export const loadSearchResults = async function (query) {
         image: recipe.image_url,
       };
     });
+    state.search.page = 1;
   } catch (error) {
     console.log(error);
     throw error;
